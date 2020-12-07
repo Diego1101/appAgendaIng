8ada42758abeab19b8ead15377790019fa272c0c
geogc98 <49170081+geogc98@users.noreply.github.com>
2020-12-07 17:08:26 -0600

    Paginas relacionadas

--- app/src/main/java/com/example/appmensajesing/registroContactos.java
+++ app/src/main/java/com/example/appmensajesing/registroContactos.java
@@ -1,8 +1,11 @@
 package com.example.appmensajesing;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class registroContactos extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class registroContactos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_contactos);
+    }
+    public void Acceso(View v){
+        Intent chk=new Intent(this, Consulta.class);
+        startActivity(chk);
     }
 }
