0c718d829a7d7dbb12571eec8a3b8ee14aec04f1
geogc98 <49170081+geogc98@users.noreply.github.com>
2020-12-13 00:00:17 -0600

    Funcionalidad acceso, detalles de contacto

--- app/src/main/java/com/example/appmensajesing/MainActivity.java
+++ app/src/main/java/com/example/appmensajesing/MainActivity.java
@@ -3,20 +3,105 @@ package com.example.appmensajesing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText nom, contra;
+    String resultados = "No hay datos";
+    String resNom = "";
+    Button acceso;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nom = (EditText) findViewById(R.id.txtAccesoUsu);
+        contra = (EditText) findViewById(R.id.txtAccesoPas);
+        acceso= (Button)findViewById(R.id.btnAccesoAceptar);
+        acceso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextUtils.isEmpty(nom.getText()) || TextUtils.isEmpty(contra.getText())){
+                    Toast.makeText(getApplicationContext(), "LLenar todos los campos", Toast.LENGTH_SHORT).show();
+                }else {
+                    acceso();
+                }
+            }
+        });
     }
 
+    public void acceso() {
+        try {
+            AsyncHttpClient cliente = new AsyncHttpClient();
+            String url = "https://agendaing.one-2-go.com/servicioWeb/contacto.php?op=acceso&";
+            RequestParams parametros = new RequestParams();
+            parametros.put("usuario",nom.getText().toString());
+            parametros.put("contra",contra.getText().toString());
 
+            cliente.post(url, parametros, new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                    //Toast.makeText(getApplicationContext(), "Entra", Toast.LENGTH_SHORT).show();
+                    mostrarDatos(new String(responseBody));
+
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                }
+            });
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void mostrarDatos(String rawData) {
+        try {
+            JSONObject data = new JSONObject(rawData);
+            resultados=data.getString("ID");
+            if(!resultados.equals("0")){
+                resNom=data.getString("NOMBRE");
+                Toast.makeText(getApplicationContext(),"Bienvenido "+ resNom, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, menuPrincipal.class);
+                startActivity(intent);
+            }else{
+                Toast.makeText(getApplicationContext(), "Acceso denegado", Toast.LENGTH_SHORT).show();
+            }
+
+            //grid.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, gridData));
+        }catch(Exception ex){
+            Toast.makeText(getApplicationContext(),"Error:"+ex.getMessage().toString(),Toast.LENGTH_SHORT).show();
+        }
+    }
     public void Menu(View v){
         Intent chk=new Intent(this,menuPrincipal.class);
         startActivity(chk);
     }
+
+
+
+
+
 }
--- app/src/main/java/com/example/appmensajesing/registroContactos.java
+++ app/src/main/java/com/example/appmensajesing/registroContactos.java
@@ -5,9 +5,11 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -16,18 +18,24 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
 public class registroContactos extends AppCompatActivity {
-    public Spinner Semestre,Carrera,Grupo;
+    public Spinner Semestre,Carrera,Grupo,Rol;
+    String resultados = "No hay datos";
     ArrayList<String> sem = new ArrayList<>();
     ArrayList<String> carr = new ArrayList<>();
     ArrayList<String> gru = new ArrayList<>();
-    ArrayAdapter<String> AdaptadorS, adapC, adapG;
-    private ArrayList<String> clavesS, clavesC, clavesG;
+    ArrayList<String> rl = new ArrayList<>();
+    ArrayAdapter<String> AdaptadorS, adapC, adapG, adapRol;
+    private ArrayList<String> clavesS, clavesC, clavesG, clavesR;
+    EditText nom, ape, us,con,tel, mail;
+    Integer car,sm,gr,rol;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,15 +43,73 @@ public class registroContactos extends AppCompatActivity {
         Semestre=(Spinner)findViewById(R.id.spRegistroSemestre);
         Carrera=(Spinner)findViewById(R.id.spRegistroCarrera);
         Grupo=(Spinner)findViewById(R.id.spRegistroGrupo);
+        Rol=(Spinner)findViewById(R.id.spRol);
+        nom =(EditText) findViewById(R.id.txtRegistroNombre);
+        ape=(EditText) findViewById(R.id.txtRegisrtroApellido);
+        us=(EditText) findViewById(R.id.txtRegistroUsuario);
+        con=(EditText) findViewById(R.id.txtRegistroContra);
+        tel=(EditText) findViewById(R.id.txtRegistroTel);
+        mail=(EditText) findViewById(R.id.txtRegistroCorreo);
+
         llenarSemestre();
         llenarCarrera();
         llenarGrupo();
+        llenarRol();
     }
     public void consulta(View v){
         Intent chk=new Intent(this, Consulta.class);
         startActivity(chk);
     }
 
+    public void llenarRol(){
+        try {
+            AsyncHttpClient cliente = new AsyncHttpClient();
+            String url = "http://agendaing.one-2-go.com/servicioWeb/catalogos.php?";
+            RequestParams parametros = new RequestParams();
+            parametros.put("op","rol");
+            cliente.post(url, parametros, new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                    mostrarDatosR(new String(responseBody));
+                }
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                }
+            });
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Error"+ e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void mostrarDatosR(String rawData) {
+        try {
+            JSONArray data = new JSONArray(rawData);
+            clavesR= new ArrayList<>();
+            for (int i=0; i < data.length(); i++) {
+                clavesR.add(data.getJSONObject(i).getString("ID"));
+                rl.add(data.getJSONObject(i).getString("NOMBRE"));
+            }
+            adapRol= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,rl);
+            Rol.setAdapter(adapRol);
+            Rol.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    //Toast.makeText(getApplicationContext(),"datos:"+clavesS.get(position),Toast.LENGTH_SHORT).show();
+                    rol=Integer.parseInt(clavesR.get(position));
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+
+
+        }catch(Exception ex){
+            ex.printStackTrace();
+            Toast.makeText(getApplicationContext(),"Error:"+ex.getMessage().toString(),Toast.LENGTH_SHORT).show();
+        }
+    }
     public void llenarSemestre(){
         try {
             AsyncHttpClient cliente = new AsyncHttpClient();
@@ -82,7 +148,7 @@ public class registroContactos extends AppCompatActivity {
                 @Override
                 public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                     //Toast.makeText(getApplicationContext(),"datos:"+clavesS.get(position),Toast.LENGTH_SHORT).show();
-
+                    sm=Integer.parseInt(clavesS.get(position));
                 }
 
                 @Override
@@ -132,7 +198,7 @@ public class registroContactos extends AppCompatActivity {
                 @Override
                 public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                     //Toast.makeText(getApplicationContext(),"datos:"+clavesS.get(position),Toast.LENGTH_SHORT).show();
-
+                    car=Integer.parseInt(clavesC.get(position));
                 }
 
                 @Override
@@ -182,7 +248,7 @@ public class registroContactos extends AppCompatActivity {
                 @Override
                 public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                     //Toast.makeText(getApplicationContext(),"datos:"+clavesS.get(position),Toast.LENGTH_SHORT).show();
-
+                    gr=Integer.parseInt(clavesG.get(position));
                 }
 
                 @Override
@@ -198,4 +264,68 @@ public class registroContactos extends AppCompatActivity {
         }
     }
 
+    public void registrar(View v){
+        if(TextUtils.isEmpty(nom.getText()) ||TextUtils.isEmpty(ape.getText()) ||TextUtils.isEmpty(us.getText())  ||TextUtils.isEmpty(con.getText())||TextUtils.isEmpty(mail.getText())  ||TextUtils.isEmpty(tel.getText())){
+            Toast.makeText(getApplicationContext(), "Debe llenar todos los campos" , Toast.LENGTH_SHORT).show();
+        }else{
+            try {
+                AsyncHttpClient cliente = new AsyncHttpClient();
+                String url = "https://agendaing.one-2-go.com/servicioWeb/contacto.php?op=registrar&";
+                RequestParams parametros = new RequestParams();
+                parametros.put("nombre", nom.getText().toString());
+                parametros.put("ape", ape.getText().toString());
+                parametros.put("usuario",us.getText().toString());
+                parametros.put("contra",con.getText().toString());
+                parametros.put("rol","1");
+                parametros.put("tel",tel.getText().toString());
+                parametros.put("email",mail.getText().toString());
+                parametros.put("carrera", "1");
+                parametros.put("grupo", "1");
+                parametros.put("semestre","1");
+                cliente.post(url, parametros, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        mostrarDatos(obDatosJSON(new String(responseBody)));
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                    }
+                });
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Error "+ e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    public void mostrarDatos(String dat) {
+        if (!resultados.equals("0")) {
+            Toast.makeText(getApplicationContext(), "Registrado" + resultados, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Algo salió mal", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public String obDatosJSON(String response) {
+        try {
+            JSONArray jsonDatos = new JSONArray(response);
+            resultados = jsonDatos.getJSONObject(0).getString("ID");
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+        }
+        return resultados;
+    }
+
+
+    public void limpiar(){
+        nom.setText("");
+        ape.setText("");
+        us.setText("");
+        con.setText("");
+        tel.setText("");
+        mail.setText("");
+    }
 }
